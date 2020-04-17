e8bd21de47a8c3b6652c3671b84e42d3cb481636
stvn <stvn@mit.edu>
2020-04-17 12:40:09 -0700

    Consolidate data structures

--- src/cc2olx/models.py
+++ src/cc2olx/models.py
@@ -1,7 +1,10 @@
-NAMESPACE = {
-    'ims': 'http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1',
-    'lomimscc': 'http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest',
-}
+import os.path
+import re
+import zipfile
+
+from cc2olx import filesystem
+from cc2olx.settings import collect_settings
+from cc2olx.settings import MANIFEST
 
 
 class ResourceFile:
@@ -24,214 +27,244 @@ class ResourceDependency:
         )
 
 
-def parse_manifest(node, ns):
-    data = dict()
-    data['metadata'] = parse_metadata(node, ns)
-    data['organizations'] = parse_organizations(node, ns)
-    data['resources'] = parse_resources(node, ns)
-    return data
+class Cartridge:
+    def __init__(self, cartridge_file):
+        self.cartridge = zipfile.ZipFile(cartridge_file)
+        self.metadata = None
+        self.resources = None
+        self.organizations = None
+        self.version = '1.1'
+        self.file_path = cartridge_file
+        self.directory = None
+        self.ns = {}
 
-
-def parse_metadata(node, ns):
-    data = dict()
-    metadata = node.find('./ims:metadata', ns)
-    if metadata:
-        data['schema'] = parse_schema(metadata, ns)
-        data['lom'] = parse_lom(metadata, ns)
-        # parse_metadata_lom(metadata)
-    return data
-
-
-def parse_schema(node, ns):
-    schema_name = parse_schema_name(node, ns)
-    schema_version = parse_schema_version(node, ns)
-    data = {
-        'name': schema_name,
-        'version': schema_version,
-    }
-    return data
-
-
-def parse_schema_name(node, ns):
-    schema_name = node.find('./ims:schema', ns)
-    schema_name = schema_name.text
-    return schema_name
-
-
-def parse_schema_version(node, ns):
-    schema_version = node.find('./ims:schemaversion', ns)
-    schema_version = schema_version.text
-    return schema_version
-
-
-def parse_lom(node, ns):
-    data = dict()
-    lom = node.find('lomimscc:lom', ns)
-    if lom:
-        data['general'] = parse_general(lom, ns)
-        data['lifecycle'] = parse_lifecycle(lom, ns)
-        data['rights'] = parse_rights(lom, ns)
-    return data
-
-
-def parse_general(node, ns):
-    data = {}
-    general = node.find('lomimscc:general', ns)
-    if general:
-        data['title'] = parse_text(general, 'lomimscc:title/lomimscc:string', ns)
-        data['language'] = parse_text(general, 'lomimscc:language/lomimscc:string', ns)
-        data['description'] = parse_text(general, 'lomimscc:description/lomimscc:string', ns)
-        data['keywords'] = parse_keywords(general, ns)
-    return data
-
-
-def parse_text(node, lookup, ns):
-    text = None
-    element = node.find(lookup, ns)
-    if element is not None:
-        text = element.text
-    return text
-
-
-def parse_keywords(node, ns):
-    # TODO: keywords
-    keywords = []
-    return keywords
-
-
-def parse_rights(node, ns):
-    data = dict()
-    element = node.find('lomimscc:rights', ns)
-    if element:
-        data['is_restricted'] = parse_text(
-            element,
-            'lomimscc:copyrightAndOtherRestrictions/lomimscc:value',
-            ns,
+    def __repr__(self):
+        filename = os.path.basename(self.file_path)
+        text = '<{_class} version="{version}" file="{filename}" />'.format(
+            _class=type(self).__name__,
+            version=self.version,
+            filename=filename,
         )
-        data['description'] = parse_text(
-            element,
-            'lomimscc:description/lomimscc:string',
-            ns,
+        return text
+
+    def _extract(self):
+        settings = collect_settings()
+        workspace = settings['workspace']
+        path_extracted = filesystem.unzip_directory(self.file_path, workspace)
+        self.directory = path_extracted
+        manifest = os.path.join(path_extracted, MANIFEST)
+        return manifest
+
+    def _update_namespaces(self, root):
+        ns = re.match('\{(.*)\}', root.tag).group(1)
+        version = re.match('.*/(imsccv\dp\d)/', ns).group(1)
+        self.ns['ims'] = ns
+        self.ns['lomimscc'] = "http://ltsc.ieee.org/xsd/{version}/LOM/manifest".format(
+            version=version,
         )
-        # TODO: cost
-    return data
 
+    def load_manifest_extracted(self):
+        manifest = self._extract()
+        tree = filesystem.get_xml_tree(manifest)
+        root = tree.getroot()
+        self._update_namespaces(root)
+        data = self.parse_manifest(root)
+        self.metadata = data['metadata']
+        self.organizations = data['organizations']
+        self.resources = data['resources']
+        self.version = self.metadata.get('schema', {}).get('version', self.version)
+        return data
 
-def parse_lifecycle(node, ns):
-    # TODO: role
-    # TODO: entity
-    data = dict()
-    contribute_date = node.find('lomimscc:lifeCycle/lomimscc:contribute/lomimscc:date/lomimscc:dateTime', ns)
-    text = None
-    if contribute_date is not None:
-        text = contribute_date.text
-    data['contribute_date'] = text
-    return data
+    def parse_manifest(self, node):
+        data = dict()
+        data['metadata'] = self.parse_metadata(node)
+        data['organizations'] = self.parse_organizations(node)
+        data['resources'] = self.parse_resources(node)
+        return data
 
+    def parse_metadata(self, node):
+        data = dict()
+        metadata = node.find('./ims:metadata', self.ns)
+        if metadata:
+            data['schema'] = self.parse_schema(metadata)
+            data['lom'] = self.parse_lom(metadata)
+        return data
 
-def parse_organizations(node, ns):
-    data = []
-    element = node.find('ims:organizations', ns) or []
-    data = [
-        parse_organization(org_node, ns)
-        for org_node in element
-    ]
-    return data
+    def parse_schema(self, node):
+        schema_name = self.parse_schema_name(node)
+        schema_version = self.parse_schema_version(node)
+        data = {
+            'name': schema_name,
+            'version': schema_version,
+        }
+        return data
 
+    def parse_schema_name(self, node):
+        schema_name = node.find('./ims:schema', self.ns)
+        schema_name = schema_name.text
+        return schema_name
 
-def parse_organization(node, ns):
-    data = {}
-    data['identifier'] = node.get('identifier')
-    data['structure'] = node.get('structure')
-    children = []
-    for item_node in node:
-        child = parse_item(item_node, ns)
-        if len(child):
-            children.append(child)
-    if len(children):
-        data['children'] = children
-    return data
+    def parse_schema_version(self, node):
+        schema_version = node.find('./ims:schemaversion', self.ns)
+        schema_version = schema_version.text
+        return schema_version
 
+    def parse_lom(self, node):
+        data = dict()
+        lom = node.find('lomimscc:lom', self.ns)
+        if lom:
+            data['general'] = self.parse_general(lom)
+            data['lifecycle'] = self.parse_lifecycle(lom)
+            data['rights'] = self.parse_rights(lom)
+        return data
 
-def parse_item(node, ns):
-    data = {}
-    identifier = node.get('identifier')
-    if identifier:
-        data['identifier'] = identifier
-    identifierref = node.get('identifierref')
-    if identifierref:
-        data['identifierref'] = identifierref
-    title = parse_text(node, 'ims:title', ns)
-    if title:
-        data['title'] = title
-    children = []
-    for child in node:
-        child_item = parse_item(child, ns)
-        if len(child_item):
-            children.append(child_item)
-    if children and len(children):
-        data['children'] = children
-    return data
+    def parse_general(self, node):
+        data = {}
+        general = node.find('lomimscc:general', self.ns)
+        if general:
+            data['title'] = self.parse_text(general, 'lomimscc:title/lomimscc:string')
+            data['language'] = self.parse_text(general, 'lomimscc:language/lomimscc:string')
+            data['description'] = self.parse_text(general, 'lomimscc:description/lomimscc:string')
+            data['keywords'] = self.parse_keywords(general)
+        return data
 
+    def parse_text(self, node, lookup):
+        text = None
+        element = node.find(lookup, self.ns)
+        if element is not None:
+            text = element.text
+        return text
 
-def parse_resources(node, ns):
-    data = []
-    element = node.find('ims:resources', ns) or []
-    data = [
-        parse_resource(sub_element, ns)
-        for sub_element in element
-    ]
-    return data
+    def parse_keywords(self, node):
+        # TODO: keywords
+        keywords = []
+        return keywords
 
+    def parse_rights(self, node):
+        data = dict()
+        element = node.find('lomimscc:rights', self.ns)
+        if element:
+            data['is_restricted'] = self.parse_text(
+                element,
+                'lomimscc:copyrightAndOtherRestrictions/lomimscc:value',
+            )
+            data['description'] = self.parse_text(
+                element,
+                'lomimscc:description/lomimscc:string',
+            )
+            # TODO: cost
+        return data
 
-def parse_resource(node, ns):
-    data = {}
-    identifier = node.get('identifier')
-    if identifier:
-        data['identifier'] = identifier
-    _type = node.get('type')
-    if _type:
-        data['type'] = _type
-    href = node.get('href')
-    if href:
-        data['href'] = href
-    intended_use = node.get('intended_use')
-    if intended_use:
-        data['intended_use'] = intended_use
-    children = []
-    for child in node:
-        prefix, has_namespace, postfix = child.tag.partition('}')
-        tag = postfix
-        if tag == 'file':
-            child_data = parse_file(child, ns)
-        elif tag == 'dependency':
-            child_data = parse_dependency(child, ns)
-        elif tag == 'metadata':
-            child_data = parse_resource_metadata(child, ns)
-        else:
-            print('UNSUPPORTED RESOURCE TYPE', tag)
-            continue
-        if child_data:
-            children.append(child_data)
-    if children and len(children):
-        data['children'] = children
-    return data
+    def parse_lifecycle(self, node):
+        # TODO: role
+        # TODO: entity
+        data = dict()
+        contribute_date = node.find(
+            'lomimscc:lifeCycle/lomimscc:contribute/lomimscc:date/lomimscc:dateTime',
+            self.ns
+        )
+        text = None
+        if contribute_date is not None:
+            text = contribute_date.text
+        data['contribute_date'] = text
+        return data
 
+    def parse_organizations(self, node):
+        data = []
+        element = node.find('ims:organizations', self.ns) or []
+        data = [
+            self.parse_organization(org_node)
+            for org_node in element
+        ]
+        return data
 
-def parse_file(node, ns):
-    data = dict()
-    href = node.get('href')
-    resource = ResourceFile(href)
-    return resource
+    def parse_organization(self, node):
+        data = {}
+        data['identifier'] = node.get('identifier')
+        data['structure'] = node.get('structure')
+        children = []
+        for item_node in node:
+            child = self.parse_item(item_node)
+            if len(child):
+                children.append(child)
+        if len(children):
+            data['children'] = children
+        return data
 
+    def parse_item(self, node):
+        data = {}
+        identifier = node.get('identifier')
+        if identifier:
+            data['identifier'] = identifier
+        identifierref = node.get('identifierref')
+        if identifierref:
+            data['identifierref'] = identifierref
+        title = self.parse_text(node, 'ims:title')
+        if title:
+            data['title'] = title
+        children = []
+        for child in node:
+            child_item = self.parse_item(child)
+            if len(child_item):
+                children.append(child_item)
+        if children and len(children):
+            data['children'] = children
+        return data
 
-def parse_dependency(node, ns):
-    data = dict()
-    identifierref = node.get('identifierref')
-    resource = ResourceDependency(identifierref)
-    return resource
+    def parse_resources(self, node):
+        data = []
+        element = node.find('ims:resources', self.ns) or []
+        data = [
+            self.parse_resource(sub_element)
+            for sub_element in element
+        ]
+        return data
 
+    def parse_resource(self, node):
+        data = {}
+        identifier = node.get('identifier')
+        if identifier:
+            data['identifier'] = identifier
+        _type = node.get('type')
+        if _type:
+            data['type'] = _type
+        href = node.get('href')
+        if href:
+            data['href'] = href
+        intended_use = node.get('intended_use')
+        if intended_use:
+            data['intended_use'] = intended_use
+        children = []
+        for child in node:
+            prefix, has_namespace, postfix = child.tag.partition('}')
+            tag = postfix
+            if tag == 'file':
+                child_data = self.parse_file(child)
+            elif tag == 'dependency':
+                child_data = self.parse_dependency(child)
+            elif tag == 'metadata':
+                child_data = self.parse_resource_metadata(child)
+            else:
+                print('UNSUPPORTED RESOURCE TYPE', tag)
+                continue
+            if child_data:
+                children.append(child_data)
+        if children and len(children):
+            data['children'] = children
+        return data
 
-def parse_resource_metadata(node, ns):
-    # TODO: this
-    return None
+    def parse_file(self, node):
+        data = dict()
+        href = node.get('href')
+        resource = ResourceFile(href)
+        return resource
+
+    def parse_dependency(self, node):
+        data = dict()
+        identifierref = node.get('identifierref')
+        resource = ResourceDependency(identifierref)
+        return resource
+
+    def parse_resource_metadata(self, node):
+        # TODO: this
+        return None
