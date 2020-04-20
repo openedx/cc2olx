6dd41a2a9d0e9a1b509bfd4cf7077bf68af74cdd
Ned Batchelder <ned@nedbatchelder.com>
2020-04-20 08:53:25 -0400

    Catch exceptions so we can process many files

--- src/cc2olx/main.py
+++ src/cc2olx/main.py
@@ -1,5 +1,6 @@
 import logging
 import os.path
+import traceback
 
 from cc2olx.settings import collect_settings
 from cc2olx import filesystem
@@ -8,23 +9,34 @@ from cc2olx.models import Cartridge
 from cc2olx import olx
 
 
-if __name__ == '__main__':
+def convert_one_file(settings, input_file):
+    print("Converting", input_file)
+    workspace = settings['workspace']
+    filesystem.create_directory(workspace)
+    cartridge = Cartridge(input_file)
+    data = cartridge.load_manifest_extracted()
+    cartridge.normalize()
+    # print()
+    # print("=" * 100)
+    # import json; print(json.dumps(cartridge.normalized, indent=4))
+    xml = olx.OlxExport(cartridge).xml()
+    olx_filename = os.path.join(workspace, cartridge.directory + "-course.xml")
+    with open(olx_filename, "w") as olxfile:
+        olxfile.write(xml)
+    tgz_filename = os.path.join(workspace, cartridge.directory + "-onefile.tar.gz")
+    olx.onefile_tar_gz(tgz_filename, xml.encode("utf8"), "course.xml")
+
+
+def main():
     settings = collect_settings()
     logging.basicConfig(**settings['logging_config'])
     logger = logging.getLogger()
-    workspace = settings['workspace']
-    filesystem.create_directory(workspace)
     for input_file in settings['input_files']:
-        print("Converting", input_file)
-        cartridge = Cartridge(input_file)
-        data = cartridge.load_manifest_extracted()
-        cartridge.normalize()
-        # print()
-        # print("=" * 100)
-        # import json; print(json.dumps(cartridge.normalized, indent=4))
-        xml = olx.OlxExport(cartridge).xml()
-        olx_filename = os.path.join(workspace, cartridge.directory + "-course.xml")
-        with open(olx_filename, "w") as olxfile:
-            olxfile.write(xml)
-        tgz_filename = os.path.join(workspace, cartridge.directory + "-onefile.tar.gz")
-        olx.onefile_tar_gz(tgz_filename, xml.encode("utf8"), "course.xml")
+        try:
+            convert_one_file(settings, input_file)
+        except Exception:
+            traceback.print_exc()
+
+
+if __name__ == '__main__':
+    main()
