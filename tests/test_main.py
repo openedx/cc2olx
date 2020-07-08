c3442e5230fc0b91a78c4f978dd7bceb0f0a6102
0x29a <demid@opencraft.com>
2020-07-08 10:45:32 +0300

    Add test for main function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,13 @@
 import tarfile
 
-from cc2olx.main import convert_one_file
+from unittest.mock import call
+
+from cc2olx.main import convert_one_file, main
 
 
 def test_convert_one_file(settings, imscc_file):
     """
-    Tests, that ``convert_one_file`` call over ``imscc`` file results in
+    Tests, that ``convert_one_file`` call for ``imscc`` file results in
     tar.gz archive with olx course.
     """
 
@@ -20,3 +22,25 @@ def test_convert_one_file(settings, imscc_file):
     with tarfile.open(tgz_path, "r:gz") as tgz:
         for member in tgz.getmembers():
             assert tgz.extractfile(member).read().decode("utf8") == course_xml_content
+
+
+def test_main(mocker):
+    """
+    Tests, that main function tries to invoke convert function for every input file.
+    """
+
+    input_files = [
+        "file1", "file2", "file3"
+    ]
+
+    mocker.patch("cc2olx.main.parse_args")
+    settings_mock = mocker.patch("cc2olx.main.collect_settings", return_value={
+        "input_files": input_files
+    })
+
+    convert_mock = mocker.patch("cc2olx.main.convert_one_file")
+
+    main()
+
+    calls = [call(settings_mock(), input_file) for input_file in input_files]
+    convert_mock.assert_has_calls(calls)
