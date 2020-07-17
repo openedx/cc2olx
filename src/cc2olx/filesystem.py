9ed519dc6ec54427938febb1f833e77dddb1fb44
0x29a <demid@opencraft.com>
2020-07-17 13:22:52 +0300

    Use pathlib in filesystem.py

--- src/cc2olx/filesystem.py
+++ src/cc2olx/filesystem.py
@@ -1,6 +1,4 @@
 import logging
-import os
-import shutil
 import zipfile
 
 from xml.etree import ElementTree
@@ -9,12 +7,9 @@ logger = logging.getLogger()
 
 
 def create_directory(directory_path):
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
-        logger.debug(
-            "Created the folder: %s",
-            directory_path,
-        )
+    if not directory_path.exists():
+        directory_path.mkdir()
+        logger.debug("Created the folder: %s", directory_path)
 
 
 def get_xml_tree(path_src):
@@ -26,18 +21,13 @@ def get_xml_tree(path_src):
     return tree
 
 
-def strip_extension(input_path):
-    input_dir, input_file = os.path.split(input_path)
-    input_file_name = os.path.splitext(input_file)[0]
-    return input_file_name
-
-
 def unzip_directory(path_src, path_dst_base=None):
-    src_dir_path = os.path.dirname(path_src)
-    file_name_without_extension = strip_extension(path_src)
-    if not path_dst_base:
-        path_dst_base = os.path.join(src_dir_path, file_name_without_extension)
-    path_dst = os.path.join(path_dst_base, file_name_without_extension)
+    src_dir_path = path_src.parent
+    path_dst_base = path_dst_base or src_dir_path
+
+    path_dst = path_dst_base / path_src.stem
+
     with zipfile.ZipFile(path_src) as output_file:
         output_file.extractall(path_dst)
+
     return path_dst
