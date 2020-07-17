33e51e6a4afd077f6d361c812cd468c103d4732e
0x29a <demid@opencraft.com>
2020-07-17 13:44:54 +0300

    Move cli to separate file and update settings code

--- src/cc2olx/settings.py
+++ src/cc2olx/settings.py
@@ -1,121 +1,49 @@
-import argparse
-import logging
-import os
+from pathlib import Path
+
+COMMON_CARTRIDGE_FILE_EXTENSION = ".imscc"
 
 
-MANIFEST = 'imsmanifest.xml'
-RESULT_TYPE_FOLDER = 'folder'
-RESULT_TYPE_ZIP = "zip"
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='{%(filename)s:%(lineno)d} - %(message)s',
-)
-logger = logging.getLogger()
+def _is_cartridge_file(path):
+    return path.is_file() and path.suffix == COMMON_CARTRIDGE_FILE_EXTENSION
 
 
-def _parse_args():
-    parser = argparse.ArgumentParser(
-        description='This script converts imscc files into folders with all the content; in the defined folder structure.'
-    )
-    group = parser.add_mutually_exclusive_group(
-        required=True,
-    )
-    group.add_argument(
-        '-f',
-        '--file',
-        nargs=1,
-        help='Please provide the path to the imscc file.',
-    )
-    group.add_argument(
-        '-l',
-        '--list',
-        nargs='*',
-        help='Please provide the paths to a list of imscc files.',
-    )
-    group.add_argument(
-        '-d',
-        '--dir',
-        nargs=1,
-        help='Please provide the path to the directory containing imscc file(s).',
-    )
-    parser.add_argument(
-        '-ll',
-        '--loglevel',
-        nargs=1,
-        choices=[
-            'DEBUG',
-            'INFO',
-            'WARNING',
-            'ERROR',
-            'CRITICAL',
-        ],
-        help='Please provide the appropriate level to change the detail of logs. It can take one of the following values, DEBUG, INFO, WARNING, ERROR, CRITICAL as argument.',
-    )
-    parser.add_argument(
-        '-r',
-        '--result',
-        nargs=1,
-        choices=[
-            RESULT_TYPE_FOLDER,
-            RESULT_TYPE_ZIP,
-        ],
-        default=RESULT_TYPE_FOLDER,
-        help="Please provide the way in which final result has to be. It can take one of the following values, {folder}, {zip} as argument.".format(
-            folder=RESULT_TYPE_FOLDER,
-            zip=RESULT_TYPE_ZIP,
-        ),
-    )
-    args = parser.parse_args()
-    return args
+def _get_files(parsed_args):
+    """
+    Collects all Common Cartridge files from list of files and directories.
+    """
+
+    files = set()
+
+    for path in parsed_args.inputs:
+        if not path.exists():
+            raise FileNotFoundError
+
+        if _is_cartridge_file(path):
+            files.add(path)
+
+        if path.is_dir():
+            for input_file in path.iterdir():
+                if _is_cartridge_file(input_file):
+                    files.add(input_file)
+
+    return files
 
 
-def _get_files(args):
-    input_files = []
-    if args.file:
-        input_files.extend(args.file)
-    elif args.list:
-        input_files.extend(args.list)
-    elif args.dir:
-        path_folder = ''.join(args.dir)
-        for input_file in os.listdir(path_folder):
-            if input_file.endswith('.imscc'):
-                input_file_full = os.path.join(path_folder, input_file)
-                input_files.append(input_file_full)
-    return input_files
+def collect_settings(parsed_args):
+    """
+    Collects settings dictionary from argparse arguments.
+    """
 
-
-def _get_log_level(args):
-    log_level = 'INFO'
-    if args.loglevel:
-        log_level = ''.join(args.loglevel)
-    return log_level
-
-
-def _get_input(args):
-    result_type = ''
-    if args.result:
-        result_type = ''.join(args.result)
-    return result_type
-
-
-def collect_settings():
-    args = _parse_args()
-    input_files = _get_files(args)
-    log_level = _get_log_level(args)
-    output_format = _get_input(args)
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    temp_folder_name = 'tmp'
-    workspace = os.path.join(dir_path, '../..', temp_folder_name)
-    workspace = os.path.normpath(workspace)
+    input_files = _get_files(parsed_args)
+    log_level = parsed_args.loglevel
     logging_config = {
-        'level': log_level,
-        'format': '{%(filename)s:%(lineno)d} - %(message)s',
+        "level": log_level,
+        "format": "{%(filename)s:%(lineno)d} - %(message)s",
     }
     settings = {
-        'input_files': input_files,
-        'output_format': output_format,
-        'logging_config': logging_config,
-        'workspace': workspace,
+        "input_files": input_files,
+        "output_format": parsed_args.result,
+        "logging_config": logging_config,
+        "workspace": Path.cwd() / "output",
     }
     return settings
