a47e34de2b624e03d6f8ab3b89534ba53dc43c58
Shimul Chowdhury <shimul@opencraft.com>
2021-01-31 19:25:33 +0600

    Add test for Web Link Content

--- tests/test_models.py
+++ tests/test_models.py
@@ -37,7 +37,7 @@ def test_load_manifest_extracted(imscc_file, settings, temp_workspace_dir):
         "version": cartridge_version,
     }
 
-    assert len(cartridge.resources) == 11
+    assert len(cartridge.resources) == 12
     assert len(cartridge.resources[0]["children"]) == 6
     assert isinstance(cartridge.resources[0]["children"][0], ResourceFile)
 
@@ -148,6 +148,18 @@ def test_cartridge_normalize(imscc_file, settings):
                                 "identifier": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
                                 "identifierref": None,
                                 "title": "PDF Outside of Web Resources",
+                            },
+                            {
+                                "children": [
+                                    {
+                                        "identifier": "web_link_content",
+                                        "identifierref": "resource_8_web_link_content",
+                                        "title": "Web Link Content",
+                                    }
+                                ],
+                                "identifier": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+                                "identifierref": None,
+                                "title": "Web Link Content",
                             },
                         ],
                         "identifier": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
