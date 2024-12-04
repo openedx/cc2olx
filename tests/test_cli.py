26468d0dc3354e8541b81cb7d4c85dc7b006c2f9
Myhailo Chernyshov <mykhailo.chernyshov@raccoongang.com>
2024-12-04 22:47:04 +0200

    test: Relative links source validation is tested

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,7 @@
 from argparse import Namespace
+from pathlib import Path
+
+import pytest
 
 from cc2olx.cli import parse_args
 
@@ -58,3 +61,32 @@ def test_parse_args_passport_file(imscc_file, passports_csv):
         output="output",
         relative_links_source=None,
     )
+
+
+def test_parse_args_with_correct_relative_links_source(imscc_file: Path) -> None:
+    """
+    Positive input test for relative links source argument.
+    """
+    relative_links_source = "https://example.com"
+
+    parsed_args = parse_args(["-i", str(imscc_file), "-s", relative_links_source])
+
+    assert parsed_args == Namespace(
+        inputs=[imscc_file],
+        loglevel="INFO",
+        result="folder",
+        link_file=None,
+        passport_file=None,
+        output="output",
+        relative_links_source=relative_links_source,
+    )
+
+
+def test_parse_args_with_incorrect_relative_links_source(imscc_file: Path) -> None:
+    """
+    Test arguments parser detects incorrect relative links sources.
+    """
+    relative_links_source = "ws://example.com",
+
+    with pytest.raises(SystemExit):
+        parse_args(["-i", str(imscc_file), "-s", relative_links_source])
